c2572e4242a1f25a94faea01fcc7afc356b920c5
VANESSA CAROLINA PEREZ MENDOZA <VANESSA.PEREZME@correoaiep.cl>
2022-10-03 02:03:01 -0300

    Segundo QR

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -9,14 +10,48 @@ import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private alertCtrl:AlertController) {}
   
   async scan(){
     console.log("Comenzandpo a escanear");
-    const status = await BarcodeScanner.checkPermission();
+    const allowed = this.checkPermisos();
+    console.log("Status permisos:",allowed);
+    console.log(status);
     const result = await BarcodeScanner.startScan();
     if (result.hasContent) {
       console.log(result.content);
     }
+  }
+
+  checkPermisos(){
+    return new Promise(async(resolve,reject)=>{
+      const status = await BarcodeScanner.checkPermission({force:true});
+      if(status.granted){
+        resolve(true);
+      }else if(status.denied){
+        const alert = await this.alertCtrl.create({
+          header:'Sin Permisos',
+          message: 'Por favor permita el acceso a la camara en sus preferencias',
+          buttons:[
+          {
+            text: 'No',
+            role:'Cancel'
+          },
+          {
+            text:'Abrir preferencias',
+            handler:()=>{
+              BarcodeScanner.openAppSettings(),
+              resolve(false)
+            }
+          }
+        ]
+        });
+
+        await alert.present();
+
+      }else{
+        resolve(reject);
+      }
+    })
   }
 }
